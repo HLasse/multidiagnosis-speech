3533f663e00923315ce6bbfe56822d1d20fd42d8
HLasse <lasseh0310@gmail.com>
2021-09-07 14:05:07 +0200

    minor progress

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # wav2vec_finetune
+
+Test finetuning of XLSR (multilingual wav2vec 2.0) for other speech classification tasks
+Initial test: gender recognition on [this](https://zenodo.org/record/1219621#.YTcmxS2w0ws) dataset.
+
+Resources: 
+- https://github.com/pytorch/fairseq/blob/master/examples/xlmr/README.md
+- https://arxiv.org/abs/2006.13979
+- https://huggingface.co/transformers/training.html
+- https://huggingface.co/blog/fine-tune-xlsr-wav2vec2
+- https://discuss.huggingface.co/t/german-asr-fine-tuning-wav2vec2/4558/5
+- https://huggingface.co/docs/datasets/loading_datasets.html#from-local-files
+- https://github.com/huggingface/transformers/blob/master/examples/research_projects/wav2vec2/FINE_TUNE_XLSR_WAV2VEC2.md
+
+Notes:
+- Look into SpecAugment for finetuning: https://arxiv.org/abs/1904.08779
+- How to make the prediction? 
+  - Feed sequence of length N to LSTM?
+  - Single FF layer on top - one prediction for each timestep (how long is a timestep?)
+
+
+TODO:
+
+Preprocessing:
+- Only include the neutral emotion (Neutre folder)
+- Preprocess data to be correct input format (16khz)
+- Format to huggingface dataset loader: {"file" : filepath_i, "label" : gender_i}
+  
+Training:
+- Determine length of timestep
+- Investigate layers
+- Make loss function (binary cross-entropy?)
+- Train!
---
+++ preproc.py
@@ -0,0 +1,44 @@
+import librosa
+import numpy as np
+import torchaudio
+import os
+import datasets
+
+import json
+
+data = os.path.join(os.getcwd(), "data", "Neutre")
+
+filepaths = os.listdir(data)
+file_dict = [{"file": os.path.join(data, path)} for path in filepaths]
+
+# save to json file to we can use hugginface datasets loader
+# seems pretty stupid
+for i, file in enumerate(file_dict):
+     with open(f"json_files/sample_{i}.json", "w") as outfile:
+         json.dump(file, outfile)
+
+
+def get_gender(filepath):
+    """files are coded so first two characters decide gender.
+    odd = man, even = woman
+    code woman as 1, men as 0"""
+    if int(filepath.split("/")[-1][0:2]) % 2 == 0:
+        return 1
+    return 0
+
+
+# Loader for datafiles
+def speech_file_to_array(batch):
+    speech_array, sampling_rate = torchaudio.load(batch["path"])
+    batch["speech"] = speech_array[0].numpy()
+    batch["sampling_rate"] = sampling_rate
+    batch["label"] = get_gender(batch["path"])
+    return batch
+
+# resample audio to 16 khz
+def resample(batch, target_sr = 16_000):
+    batch["speech"] = librosa.resample(np.asarray(batch["speech"]), batch["sampling_rate"], target_sr)
+    return batch
+
+# remember to save preprocessed dataset to disk (dataset.save_to_disk)
+dataset = datasets.load_dataset("json", data_files=[os.path.join("json_files", file) for file in os.listdir("json_files")], split="train")
