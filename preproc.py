babe49a9e4d3d58bcef4905aba336ca8cc5eb060
HLasse <lasseh0310@gmail.com>
2021-11-04 15:28:07 +0100

    create unique ids and duration check

--- preproc.py
+++ preproc.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 from pathlib import Path
 import re
 
+import librosa
+
 BOTH_GENDERS = False
 SAVE_IDS = True
 ID_SAVE_PATH = os.path.join("data", "id_train_test_split_males.csv")
@@ -17,16 +19,18 @@ def get_metadata(filename):
     file = Path(filename)
     group = file.parent.parent.parent.name
     if group in ["ASD", "SCHZ"]:
+        study = re.findall("Study(\d)", file.name)[0]
         id = re.findall("S(\d\d\d)", file.name)
         trial = re.findall("T(\d)", file.name)
     if group == "DEPR":
+        study = "1"
         id = re.findall("d[pc](\d+)", file.name)
         trial = re.findall("_(\d+)", file.name)
     if not id:
         ValueError(f"No ID found in filename {file}")
     if not trial:
         ValueError(f"No trial found in filename {file}")
-    return (id[0], trial[0])
+    return (id[0], trial[0], study)
 
 
 def populate_data_dict(dirs, data_dict):
@@ -48,18 +52,21 @@ def populate_data_dict(dirs, data_dict):
             files = get_files_from_dir(diagnosis / "TD")
             data_dict = update_data_dict(data_dict, files, "TD")
     # flatten
-    data_dict = {k : flatten(v) for k, v in data_dict.items()}
+    data_dict = {k: flatten(v) for k, v in data_dict.items()}
     return data_dict
+
 
 def update_data_dict(data_dict, files, label):
     """update metadata dictionary based on information in filename"""
     data_dict["file"].append(files)
     data_dict["label"].append([label] * len(files))
     data_dict["origin"].append([files[0].split("/")[-4]] * len(files))
-    ids, trials = zip(*[get_metadata(f) for f in files])
+    ids, trials, study = zip(*[get_metadata(f) for f in files])
     data_dict["id"].append(ids)
     data_dict["trial"].append(trials)
+    data_dict["study"].append(study)
     return data_dict
+
 
 def get_files_from_dir(path):
     """get files of interest"""
@@ -67,25 +74,56 @@ def get_files_from_dir(path):
     files = os.listdir(wd)
     files = [os.path.join(wd, f) for f in files]
     return files
-    
+
 
 def flatten(t):
     """flatten list"""
     return [item for sublist in t for item in sublist]
 
 
+def create_unique_ids(df: pd.DataFrame) -> pd.DataFrame:
+    """Creates new IDs by adding the origin, label, and study to the id
+
+    Args:
+        df (pd.DataFrame): DataFrame with id, label, and origin columns
+
+    Returns:
+        pd.DataFrame: Original dataframe with updated id column
+    """
+
+    df["id"] = df["origin"] + "_" + df["label"] + "_" + df["study"] + "_" + df["id"]
+    return df
+
+
+def audiofile_duration(path):
+    """Get duration of file in seconds"""
+    return librosa.get_duration(filename=path)
+
+
 if __name__ == "__main__":
     data_dirs = os.path.join(os.getcwd(), "data", "multi_diagnosis")
-    data_dict = {"file" : [], "label" : [], "origin" : [], "id" : [], "trial" : []}
+    data_dict = {
+        "file": [],
+        "label": [],
+        "origin": [],
+        "id": [],
+        "trial": [],
+        "study": [],
+    }
     data_dict = populate_data_dict(data_dirs, data_dict)
-    
+
     df = pd.DataFrame(data_dict)
 
     #  check number of participants in each group + distribution of number of files for each
-    df.groupby(["origin", "label", "id"]).count()["file"].groupby(["origin", "label"]).describe()
+    df = create_unique_ids(df)
+    df.groupby(["id", "origin", "label", "study"]).count()["file"].groupby(
+        ["origin", "label"]
+    ).describe()
+
+    # check length of audio files
+    df["duration"] = df["file"].apply(audiofile_duration)
+    df.groupby(["origin", "label"]).describe()["duration"]
 
     ### TODO
     # make train/val/test split with Roberta and Riccardo or set up IDS for CV
-    # 
-
-
+    #
