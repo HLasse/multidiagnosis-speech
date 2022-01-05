bba416ded49a3423aa09e371de90778cab9815f3
HLasse <lasseh0310@gmail.com>
2022-01-05 12:24:59 +0100

    add: evaluation script on file level

--- evaluate.py
+++ evaluate.py
@@ -1,9 +1,12 @@
 """evaluate model performance
 TODO
 - Evaluate by window and by participant (rewrite to make windows)
+- make more efficient by not using apply
+    - Make windows first, then use predict function on 
 """
 
 import os
+import sys
 import numpy as np
 import pandas as pd
 
@@ -11,21 +14,54 @@ import torch
 import torch.nn.functional as F
 import torchaudio
 
-from transformers import (AutoConfig, 
-    Wav2Vec2FeatureExtractor)
+from typing import Union
+
+import time
+
+from transformers import AutoConfig, Wav2Vec2FeatureExtractor, HfArgumentParser
 from src.processor import CustomWav2Vec2Processor
 from src.model import Wav2Vec2ForSequenceClassification
 from src.make_windows import stack_frames
 
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 
-MODEL_PATH = os.path.join("model", "xlsr_autism_stories", "checkpoint-470")
-TEST = pd.read_csv(os.path.join("data", "splits", "stories_test_data_gender_False.csv"))
-LABEL_COL = "Diagnosis"
+import dataclasses
+from dataclasses import dataclass, field
 
-USE_WINDOWING = True
-WINDOW_LENGTH = 5
-STRIDE_LENGTH = 1
+
+@dataclass
+class EvalArguments:
+    model_path: str = field(default="", metadata={"help": "path to model"})
+    data_path: str = field(
+        default="data/audio_file_splits/audio_val_split.csv",
+        metadata={"help": "path to data to evaluate"},
+    )
+    input_col: str = field(
+        default="file", metadata={"help": "name of column in csv with input file"}
+    )
+    label_col: str = field(
+        default="label", metadata={"help": "name of column in csv with label"}
+    )
+    save_dir: str = field(
+        default="data/eval_results",
+        metadata={
+            "help": "path to save results. Uses model name as filename if not specified"
+        },
+    )
+    use_windowing: bool = field(
+        default=True, metadata={"help": "segment files into windows"}
+    )
+    window_length: int = field(default=4, metadata={"help": "window length in seconds"})
+    stride_length: Union[int, float] = field(
+        default=1.0, metadata={"help": "stride length in seconds"}
+    )
+    performance_by_window: bool = field(
+        default=True, metadata={"help": "evaluate performance by window"}
+    )
+    performance_by_id: bool = field(
+        default=True, metadata={"help": "evaluate performance by participant"}
+    )
+
 
 def speech_file_to_array_fn(path, sampling_rate):
     speech_array, _sampling_rate = torchaudio.load(path)
@@ -36,15 +72,23 @@ def speech_file_to_array_fn(path, sampling_rate):
 
 def stack_speech_file_to_array(path):
     speech_array, sampling_rate = torchaudio.load(path)
-    windowed_arrays = stack_frames(speech_array.squeeze(), sampling_rate=sampling_rate,
-                        frame_length=WINDOW_LENGTH, frame_stride=STRIDE_LENGTH)
     resampler = torchaudio.transforms.Resample(sampling_rate, target_sampling_rate)
-    windowed_arrays = [resampler(window).squeeze() for window in windowed_arrays]
+    speech_array = resampler(speech_array)
+
+    windowed_arrays = stack_frames(
+        speech_array.squeeze(),
+        sampling_rate=sampling_rate,
+        frame_length=eval_args.window_length,
+        frame_stride=eval_args.stride_length,
+    )
     return windowed_arrays
+
 
 def predict(path, sampling_rate):
     speech = speech_file_to_array_fn(path, sampling_rate)
-    features = processor(speech, sampling_rate=sampling_rate, return_tensors="pt", padding=True)
+    features = processor(
+        speech, sampling_rate=sampling_rate, return_tensors="pt", padding=True
+    )
 
     input_values = features.input_values.to(device)
     attention_mask = features.attention_mask.to(device)
@@ -57,47 +101,78 @@ def predict(path, sampling_rate):
     confidence = scores[np.argmax(scores)]
     return pred, confidence
 
+
 def predict_windows(path, sampling_rate, aggregation_fn=lambda x: np.mean(x, axis=0)):
     """Create windows from an input file and output aggregated predictions"""
     speech_windows = stack_speech_file_to_array(path)
-    features = processor(speech_windows, sampling_rate=sampling_rate, return_tensors="pt", padding=True)
-    
-    input_values = features.input_values.to(device)
-    attention_mask = features.attention_mask.to(device)
+    features = processor(
+        speech_windows, sampling_rate=sampling_rate, return_tensors="pt", padding=True
+    )
+
+    # needs to remove first dimension if more than one window
+    # squeeze doesn't work if only 1 window (removes two dimensions)
+    input_values = features.input_values.to(device).flatten(0, 1)
+    attention_mask = features.attention_mask.to(device).flatten(0, 1)
 
     with torch.no_grad():
-        logits = model(input_values, attention_mask=attention_mask).logits    
-    
+        logits = model(input_values, attention_mask=attention_mask).logits
+
     scores = [F.softmax(logit, dim=0).detach().cpu().numpy() for logit in logits]
     pooled_pred = aggregation_fn(scores)
     pred = config.id2label[np.argmax(pooled_pred)]
     confidence = pooled_pred[np.argmax(pooled_pred)]
     return pred, confidence
 
-def add_predicted_and_confidence(df, use_windowing=USE_WINDOWING):
-    if use_windowing:
-        pred, confidence = predict_windows(df["file"], target_sampling_rate)
+
+def add_predicted_and_confidence(df):
+    if eval_args.use_windowing:
+        pred, confidence = predict_windows(
+            df[eval_args.input_col], target_sampling_rate
+        )
     else:
-        pred, confidence = predict(df["file"], target_sampling_rate)
+        pred, confidence = predict(df[eval_args.input_col], target_sampling_rate)
     df["pred"] = pred
     df["confidence"] = confidence
     return df
 
-# setup model
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-config = AutoConfig.from_pretrained(MODEL_PATH)
-feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(MODEL_PATH)
-processor = CustomWav2Vec2Processor(feature_extractor=feature_extractor)
-target_sampling_rate = processor.feature_extractor.sampling_rate
-model = Wav2Vec2ForSequenceClassification.from_pretrained(MODEL_PATH).to(device)
 
-# load test data
+if __name__ == "__main__":
 
+    parser = HfArgumentParser((EvalArguments))
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        # loading parameters from json config file if supplied
+        eval_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    else:
+        eval_args = parser.parse_args_into_dataclasses()
 
-# apply predictions
-test = TEST.apply(add_predicted_and_confidence, axis=1)
+    # turned into a tuple with 1 element for some reason, fix it at some point
+    eval_args = eval_args[0]
+    # setup model
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    config = AutoConfig.from_pretrained(eval_args.model_path)
+    feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(eval_args.model_path)
+    processor = CustomWav2Vec2Processor(feature_extractor=feature_extractor)
+    target_sampling_rate = processor.feature_extractor.sampling_rate
+    model = Wav2Vec2ForSequenceClassification.from_pretrained(eval_args.model_path).to(
+        device
+    )
 
-print(confusion_matrix(test[LABEL_COL], test["pred"]))
-print(classification_report(test[LABEL_COL], test["pred"]))
-acc = accuracy_score(test[LABEL_COL], test["pred"])
-print(f"accuracy: {acc}")
+    # load test data
+    ## turn into huggingface dataset
+    ### make windows with map
+    ### apply model on windows with batched map
+
+    test = pd.read_csv(eval_args.data_path)
+    print(f"Evaluating on {eval_args.data_path} containing {len(test)} files")
+    # apply predictions
+    t0 = time.time()
+    test = test.apply(add_predicted_and_confidence, axis=1)
+    print(f"Time taken: {time.time() - t0} for {len(test)} files")
+
+    print(confusion_matrix(test[eval_args.label_col], test["pred"]))
+    print(classification_report(test[eval_args.label_col], test["pred"]))
+    acc = accuracy_score(test[eval_args.label_col], test["pred"])
+    print(f"accuracy: {acc}")
+
+    # save results
+    ## if eval_args.save_dir does not have a csv ending, use the model name + csv
