fe11db2bd34b141c3edad910ad633e4bbd703866
HLasse <lasseh0310@gmail.com>
2022-01-20 13:49:18 +0100

    add: debug from root

--- run_preprocessing.sh
+++ run_preprocessing.sh
@@ -1,4 +1,8 @@
+echo "Running make_train_val_test_split.py"
 python preprocessing/make_train_val_test_split.py
+echo "Running preproc_audio.py"
 python preprocessing/preproc_audio.py
+echo "Running make_dummy_audio_files.py"
 python preprocessing/make_dummy_audio_files.py
+echo "Running make_binary_prediction_splits.py"
 python preprocessing/make_binary_prediction_splits.py
