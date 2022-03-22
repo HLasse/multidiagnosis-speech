cbb57a023d269e0c6156a2c8dbdfb5c91ace872e
Lasse Hansen <lasseh0310@gmail.com>
2022-03-22 15:04:05 +0100

    add git to setup

--- ucloud_setup.sh
+++ ucloud_setup.sh
@@ -13,3 +13,6 @@ pip install soxbindings
 pip install -r requirements.txt
 
 pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+git config --global user.email "lasseh0310@gmail.com"
+git config --global user.name "Lasse Hansen"
